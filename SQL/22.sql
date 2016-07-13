4db048bad9a4a6e88731e55619b8b47031df58ae
Настя <radagast@localhost.localdomain>
2016-07-13 17:49:51 +0300

    The second SQL task

---
+++ SQL/22.sql
@@ -0,0 +1,7 @@
+SELECT * FROM pers
+WHERE TO_NUMBER(SUBSTR(TO_CHAR(HIRE_DATE),1,2))<5;
+
+SELECT TO_DATE(TO_CHAR(hire_date, 'fmDD-MON')||'-2010') as "Первое поле", 
+REPLACE(TO_CHAR(TRUNC(hire_date, 'DD'), 'fmYYYY.MM.DD'), '.', '-') as "Второе поле" 
+FROM pers
+WHERE TO_NUMBER(SUBSTR(TO_CHAR(HIRE_DATE),1,2))<5;
