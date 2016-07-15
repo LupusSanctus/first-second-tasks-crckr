9c823fc16140456db4778006f5111e5b97ec6208
Настя <radagast@localhost.localdomain>
2016-07-15 18:44:34 +0300

    Fexed identification erro

--- SQL/32.sql
+++ SQL/32.sql
@@ -1,11 +1,12 @@
 SELECT * FROM
   CONTR LEFT OUTER JOIN PERS ON (CONTR.PERS_ID = PERS.PERS_ID)
-  INNER JOIN (SELECT depart.depart_id AS deprt, depart.parent_id, depart.name, depart.min_salary, depart.max_salary, depart.director_id FROM DEPART WHERE 1500 BETWEEN 
-  DEPART.MIN_SALARY AND DEPART.MAX_SALARY)
-    ON (deprt = contr.depart_id);
+  INNER JOIN (SELECT * FROM DEPART WHERE 1500 BETWEEN DEPART.MIN_SALARY AND DEPART.MAX_SALARY) d
+    ON (d.depart_id = contr.depart_id);
 
 SELECT * FROM
   CONTR LEFT OUTER JOIN PERS ON (CONTR.PERS_ID = PERS.PERS_ID),
-    (SELECT depart.depart_id AS deprt, depart.parent_id, depart.name, depart.min_salary, depart.max_salary, depart.director_id FROM DEPART
-    WHERE 1500 BETWEEN DEPART.MIN_SALARY AND DEPART.MAX_SALARY)
-    WHERE CONTR.DEPART_ID = deprt;
+  (SELECT * FROM DEPART WHERE 1500 BETWEEN DEPART.MIN_SALARY AND DEPART.MAX_SALARY) d
+    WHERE CONTR.DEPART_ID = d.DEPART_ID;
+    
+    
+    
