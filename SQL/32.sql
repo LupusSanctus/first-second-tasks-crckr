2deaa94d19f7883ac71eda41e78a0ea81f67e735
Настя <radagast@localhost.localdomain>
2016-07-15 18:27:54 +0300

    Fixed identification error (depart.depart_id)

--- SQL/32.sql
+++ SQL/32.sql
@@ -1,9 +1,11 @@
 SELECT * FROM
   CONTR LEFT OUTER JOIN PERS ON (CONTR.PERS_ID = PERS.PERS_ID)
-  INNER JOIN (SELECT * FROM DEPART WHERE 1500 BETWEEN DEPART.MIN_SALARY AND DEPART.MAX_SALARY)
-    ON (depart.depart_id = contr.depart_id);
+  INNER JOIN (SELECT depart.depart_id AS deprt, depart.parent_id, depart.name, depart.min_salary, depart.max_salary, depart.director_id FROM DEPART WHERE 1500 BETWEEN 
+  DEPART.MIN_SALARY AND DEPART.MAX_SALARY)
+    ON (deprt = contr.depart_id);
 
 SELECT * FROM
   CONTR LEFT OUTER JOIN PERS ON (CONTR.PERS_ID = PERS.PERS_ID),
-  (SELECT * FROM DEPART WHERE 1500 BETWEEN DEPART.MIN_SALARY AND DEPART.MAX_SALARY)
-    WHERE CONTR.DEPART_ID = DEPART.DEPART_ID;
+    (SELECT depart.depart_id AS deprt, depart.parent_id, depart.name, depart.min_salary, depart.max_salary, depart.director_id FROM DEPART
+    WHERE 1500 BETWEEN DEPART.MIN_SALARY AND DEPART.MAX_SALARY)
+    WHERE CONTR.DEPART_ID = deprt;
